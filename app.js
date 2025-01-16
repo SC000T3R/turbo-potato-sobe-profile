df3e240c08151ceff81ec5d8a97b9212e6893138
Hester <lhester7@una.edu>
2025-01-16 16:37:39 -0600

    here I am, once again from the internet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000;
 
 
 //endpoint, middleware(s)
@@ -7,4 +8,4 @@ app.get('/', function (req, res) {
   res.send('Hello Express')
 })
 
-app.listen(3000)
+app.listen(port, ()=> console.log(` server is running on ... ${port}`));
