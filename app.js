f29be31aa7c8f8e5ca9143b5268dcb7c8b74613d
Hester <lhester7@una.edu>
2025-01-21 16:43:55 -0600

    how to do stuff with static file

--- app.js
+++ app.js
@@ -2,10 +2,32 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + `/public`))
 
-//endpoint, middleware(s)
+
 app.get('/', function (req, res) {
-  res.send('Hello Express')
+  res.sendFile('index.html')
 })
 
-app.listen(port, ()=> console.log(` server is running on ... ${port}`));
+app.get('/nodemon', function (req, res) {
+  res.send('look ma, no kill process')
+})
+
+
+//endpoint, middleware(s)
+app.get('/HelloRender', function (req, res) {
+  res.send('Hello Express from Real World<br><a href="/">back to home</a>')
+})
+
+app.get('/ejs', function (req, res) {
+  res.render('indexy')
+})
+
+
+app.listen(
+  port, 
+  ()=> console.log(
+    ` server is running on ... ${port}`
+  )
+);
