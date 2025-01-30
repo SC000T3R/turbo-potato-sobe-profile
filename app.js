21e9ea64ba5cafcc192637089ca4a83cd7ae0afd
Luke Hester <lwhester5@gmail.com>
2025-01-30 13:44:52 -0600

    this is the updated work

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express')
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
-
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
 // const path = require('path)
 //app.use(express.static(__dirname + `/public`))
 
@@ -32,46 +32,66 @@ async function run() {
     await client.close();
   }
 }
-// run().catch(console.dir);
+run().catch(console.dir);
 
 
 app.get('/', function (req, res) {
   res.sendFile('index.html')
 })
 
-// app.get('/nodemon', function (req, res) {
-//   res.send('look ma, no kill process')
-// })
+app.post('/saveMyName', (req,res)=>{
+  console.log('did we hit the endpoint?');
+  
+  console.log(req.body);
+
+  res.render('indexy', {pageTitle: req.body.myName});
+})
+
+app.get('/saveMyNameGet', (req,res)=>{
+  console.log('did we hit the endpoint?');
+
+  console.log(req.query);
+
+  let reqName = req.query.myNameGet;
+
+  res.render('indexy', {pageTitle: reqName});
+})
+
+
+
+app.get('/nodemon', function (req, res) {
+  res.send('look ma, no kill process')
+})
 
 
 // //endpoint, middleware(s)
-// app.get('/HelloRender', function (req, res) {
-//   res.send('Hello Express from Real World<br><a href="/">back to home</a>')
-// })
+app.get('/HelloRender', function (req, res) {
+  res.send('Hello Express from Real World<br><a href="/">back to home</a>')
+})
 
-// app.get('/ejs', function (req, res) {
-//   res.render('indexy')
-// })
+app.get('/ejs', function (req, res) {
+  res.render('indexy', {pageTitle: 'my ejs page'});
+})
 
 
-// app.listen(
-//   port, 
-//   ()=> console.log(
-//     ` server is running on ... ${port}`
-//   )
-// );
+app.listen(
+  port, 
+  ()=> console.log(
+    `server is running on ... localhost:${port}`
+  )
+);
 
 //ejs stuff
-app.get('/ejs', async (req, res) => {
+// app.get('/ejs', async (req, res) => {
  
-  await client.connect();
-  let result = await client.db("barrys-db").collection("whatever-collection").find({}).toArray();
+//   await client.connect();
+//   let result = await client.db("barrys-db").collection("whatever-collection").find({}).toArray();
 
-  console.log(result);
+//   console.log(result);
 
-  res.render('index', {
-    ejsResult : result
-  });
-});
+//   res.render('index', {
+//     ejsResult : result
+//   });
+// });
 
-app.listen(3000)
+// app.listen(3000)
