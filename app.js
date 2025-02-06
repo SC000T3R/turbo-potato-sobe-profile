3cbca1051621215008dde820e210e48f0b29a4b9
Hester <lhester7@una.edu>
2025-02-06 16:42:00 -0600

    you need to update this

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 
 const express = require('express')
 require('dotenv').config()
-const shajs = require('sha.js')
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4444;
 const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // const path = require('path)
 //app.use(express.static(__dirname + `/public`))
 
@@ -22,7 +21,7 @@ const client = new MongoClient(uri, {
   }
 })
 
-console.log(shajs('sha256').update('cat').digest('hex'));
+// console.log(shajs('sha256').update('cat').digest('hex'));
 
 async function run() {
   try {
@@ -38,49 +37,108 @@ async function run() {
 }
 // run().catch(console.dir);
 
-async function getData() {
-  await client.connect();
-  let collection = await client.db("guitar-app-database").collection("guitar-app-songs");
+// async function getData() {
+//   await client.connect();
+//   let collection = await client.db("luke-turbo-potato-sobe-profile").collection("luke-turbo-potato-sobe-profile");
 
-  let results = await collection.find({}).toArray();
+//   let results = await collection.find({}).toArray();
 
-  console.log(results);
-  return results;
+//   console.log(results);
+//   return results;
 
-}
+// }
 
 app.get('/read', async function (req, res) {
   let getDataResults = await getData();
   console.log(getDataResults);
-  res.render('songs', 
-    { songData : getDataResults} );
+  res.render('songs',
+    { songData: getDataResults });
+
+})
+
+
+app.post('/insert', async (req, res) => {
+
+  console.log('in /insert');
+
+  let newSong = req.query.myName;
+  console.log(newSong);
+
+  await client.connect();
+
+  await client
+    .db('luke-turbo-potato-sobe-profile')
+    .collection('luke-turbo-potato-sobe-profile')
+    .insertOne({ song: newSong });
+  res.redirect('/read');
+})
+
+app.post('/update', async (req, res) => {
+
+  console.log("req.body: ", req.body)
+
+  client.connect;
+  const collection = client.db("luke-turbo-potato-sobe-profile").collection("song-collection");
+  let result = await collection.findOneAndUpdate(
+    { "_id": new ObjectId(req.body.nameID) }, { $set: { "fname": req.body.inputUpdateName } }
+  )
+    .then(result => {
+      console.log(result);
+      res.redirect('/');
+    })
+});
+
+
+app.post('/delete/:id', async (req, res) => {
+
+  console.log("in delete, req.parms.id: ", req.params.id)
+
+  client.connect;
+  const collection = client.db("barrys-db").collection("whatever-collection");
+  let result = await collection.findOneAndDelete(
+    { "_id": new ObjectId(req.params.id) }).then(result => {
+      console.log(result);
+      res.redirect('/');
+    })
+
+
 
 })
 
 
 
+
+
 async function getData() {
-  
+
   await client.connect();
   let collection = await client.db("luke-turbo-potato-sobie-profile").collection("luke-turbo-potato-sobie-profile");
-  
+
   let results = await collection.find({}).toArray();
-  
+
   console.log(results);
 
   return results;
-  
-    // res.send(results).status(200);
+
+  // res.send(results).status(200);
 
 }
 
-getData();
 
-app.get('/read', async function (req, res) {
-  let getDataResults = await getData();
-  console.log(getDataResults);
-  res.render('songs', { songData : getDataResults} );
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -90,22 +148,22 @@ app.get('/', function (req, res) {
   res.sendFile('index.html')
 })
 
-app.post('/saveMyName', (req,res)=>{
+app.post('/saveMyName', (req, res) => {
   console.log('did we hit the endpoint?');
-  
+
   console.log(req.body);
 
-  res.render('indexy', {pageTitle: req.body.myName});
+  res.render('indexy', { pageTitle: req.body.myName });
 })
 
-app.get('/saveMyNameGet', (req,res)=>{
+app.get('/saveMyNameGet', (req, res) => {
   console.log('did we hit the endpoint?');
 
   console.log(req.query);
 
   let reqName = req.query.myNameGet;
 
-  res.render('indexy', {pageTitle: reqName});
+  res.render('indexy', { pageTitle: reqName });
 })
 
 
@@ -121,20 +179,20 @@ app.get('/helloRender', function (req, res) {
 })
 
 app.get('/ejs', function (req, res) {
-  res.render('indexy', {pageTitle: 'my ejs page'});
+  res.render('indexy', { pageTitle: 'my ejs page' });
 })
 
 
 app.listen(
-  port, 
-  ()=> console.log(
+  port,
+  () => console.log(
     `server is running on ... localhost:${port}`
   )
 );
 
 //ejs stuff
 // app.get('/ejs', async (req, res) => {
- 
+
 //   await client.connect();
 //   let result = await client.db("barrys-db").collection("whatever-collection").find({}).toArray();
 
