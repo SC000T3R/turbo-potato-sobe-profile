a0ef53f8421d23ff493c0a1c562778f30d1afe7d
Luke Hester <lwhester5@gmail.com>
2025-02-27 21:23:45 -0600

    fixes2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 require('dotenv').config()
-const { MongoCLient, ServerApiVersion, ObjectId } = require('mongodb');
+const {MongoClient, ObjectId } = require('mongodb');
 const app = express()
 const uri = process.env.MONGO_URI;
 const port = process.env.PORT || 4444;
@@ -39,7 +39,7 @@ app.get('/', async function (req, res) {
   res.render('indexy', 
     { profileData : results} ); 
 
-})
+});
 
 app.post('/insert', async (req,res)=> {
 
